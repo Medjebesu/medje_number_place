89683641ceacf2687ea1ab494d496c43b2731a72
Medjebesu <develop@medjeworks.net>
2023-12-08 14:35:42 +0900

    RecoilRootの設定箇所をルートトップに変更

--- medje-numberplace/src/App.tsx
+++ medje-numberplace/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {GameDisp} from './medjeNumberplace';
 
+import { RecoilRoot } from 'recoil'
+
 function App() {
 
   return (
-    <>
+    <RecoilRoot>
       <GameDisp/>
-    </>
+    </RecoilRoot>
   )
 }
 
