b41300a90209032e3c6e71f26850c7dd27e463b1
Medjebesu <develop@medjeworks.net>
2024-02-02 19:39:36 +0900

    定義ファイルの追跡除外

--- medje-numberplace/src/messagingCtrl/definition.tsx
+++
@@ -1,8 +0,0 @@
-// 接続先情報定義
-const schema = "http:"
-const requestDomain = "localhost";
-
-const requestPort = "55555";
-
-export const baseUrl =  (!requestPort) ? schema + "//" + requestDomain : 
-                                         schema + "//" + requestDomain + ":" + requestPort;
